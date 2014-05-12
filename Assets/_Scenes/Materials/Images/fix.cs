e00162bb04ef503059a058453440824903b07f09
Stamenov96 <stefan.stamenov1996@gmail.com>
2014-05-12 23:13:46 +0300

    firstBackground

---
+++ Assets/_Scenes/Materials/Images/fix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fix : MonoBehaviour
+{
+	public float m_NativeRatio = 1.3333333333333F;
+	
+	void Start ()
+	{
+		float currentRatio = (float)Screen.width / (float)Screen.height;
+		Vector3 scale = transform.localScale;
+		scale.x *= m_NativeRatio / currentRatio;
+		transform.localScale = scale;
+	}
+	
+}
